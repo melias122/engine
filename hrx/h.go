01933629e529a9c3927f3471aaa697479706f637
Martin Elias <melias122@gmail.com>
2015-06-03 19:57:33 +0200

    male zmeny v maine

--- hrx/h.go
+++ hrx/h.go
@@ -12,7 +12,7 @@ import (
 type H struct {
 	m   int
 	max int
-	sk  map[int]int      // pocetnost, pocet cisel
+	sk  map[int]int      // pocetnost(skupina), pocet cisel
 	get func(*num.N) int // cislo -> pocet
 }
 
@@ -26,19 +26,50 @@ func New(m int, f func(*num.N) int) *H {
 	return h
 }
 
-func (h *H) Add(n *num.N) {
-	p := h.get(n)
-	if p > 0 {
-		if h.sk[p-1] > 1 {
-			h.sk[p-1]--
+func (h *H) add(sk, pocet int) {
+	// del := false
+	if sk > 0 {
+		hsk := h.sk[sk-1]
+		if hsk-pocet >= 1 {
+			h.sk[sk-1] = hsk - pocet
 		} else {
-			delete(h.sk, p-1)
+			delete(h.sk, sk-1)
+			// del = true
 		}
 	}
-	h.sk[p]++
-	if p > h.max {
-		h.max = p
+	h.sk[sk] = h.sk[sk] + pocet
+	if sk > h.max {
+		h.max = sk
 	}
+}
+
+func (h *H) del(sk, pocet int) {
+	// Z aktualnej sk odratam pocet
+	// ak je 0 zmazem sk, ak sk bola max
+	// treba preratat nove max
+	del := false
+	hsk := h.sk[sk]
+	if hsk-pocet >= 1 {
+		h.sk[sk] = hsk - pocet
+	} else {
+		delete(h.sk, sk)
+		del = true
+	}
+	// do predoslej sk priratam pocet
+	h.sk[sk-1] = h.sk[sk-1] + pocet
+	if del {
+		h.max = 0
+		for max := range h.sk {
+			if max > h.max {
+				h.max = max
+			}
+		}
+	}
+}
+
+func (h *H) Add(n *num.N) {
+	sk := h.get(n)
+	h.add(sk, 1)
 }
 
 func (h *H) Get() float64 {
@@ -47,11 +78,27 @@ func (h *H) Get() float64 {
 	}
 	var hrx float64
 	for k, v := range h.sk {
+
 		hrx += ((float64(v) / float64(h.m)) *
 			math.Pow((float64(h.max)-float64(k))/float64(h.max), 16))
 	}
 	hrx = math.Pow(hrx, 0.25)
 	hrx *= 100
+	return hrx
+}
+
+func (h *H) Simul(p Presun) float64 {
+	// z aktualnej skupiny potrebujem preniest t.Max
+	// do dalsej skupiny sk+1
+	for _, t := range p {
+		h.add(t.Sk+1, t.Max)
+	}
+	// Vypocitaj hrx pre zostavu p
+	hrx := h.Get()
+	// Obnov povodny stav
+	for _, t := range p {
+		h.del(t.Sk+1, t.Max)
+	}
 	return hrx
 }
 
@@ -65,8 +112,8 @@ func (h *H) Presun() Presun {
 }
 
 type Tab struct {
-	sk  int
-	max int
+	Sk  int
+	Max int
 }
 
 type Presun []Tab
@@ -80,15 +127,15 @@ func (p Presun) copy() Presun {
 }
 
 func (p Presun) Len() int           { return len(p) }
-func (p Presun) Less(i, j int) bool { return p[i].sk < p[j].sk }
+func (p Presun) Less(i, j int) bool { return p[i].Sk < p[j].Sk }
 func (p Presun) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func (p Presun) String() string {
 	if len(p) > 0 {
-		s := make([]int, p[len(p)-1].sk+1)
+		s := make([]int, p[len(p)-1].Sk+1)
 		s[0] = 0
 		for _, v := range p {
-			s[v.sk] = v.max
+			s[v.Sk] = v.Max
 		}
 		s2 := make([]string, len(s))
 		for i := range s {
@@ -117,12 +164,12 @@ func generujPresun(t []Tab, n int, p Presun, ch chan Presun) {
 		return
 	}
 
-	max := t[0].max
+	max := t[0].Max
 	if max > n {
 		max = n
 	}
 	for max > 0 {
-		p = append(p, Tab{t[0].sk, max})
+		p = append(p, Tab{t[0].Sk, max})
 		if n-max > 0 {
 			generujPresun(t[1:], n-max, p, ch)
 		} else {
--- hrx/h_test.go
+++ hrx/h_test.go
@@ -2,7 +2,7 @@ package hrx
 
 import "testing"
 
-func TestHrx(t *testing.T) {
+func TestGenerujPresun(t *testing.T) {
 	// sk := []Tab{
 	// 	{1, []int{8}},
 	// 	{2, []int{6, 13, 16, 18, 31, 35}},
@@ -14,24 +14,91 @@ func TestHrx(t *testing.T) {
 	// 	{8, []int{3, 20, 32}},
 	// 	{10, []int{7}},
 	// }
-	sk := []Tab{
-		{1, 1},
-		{2, 6},
-		{4, 4},
-		{3, 10},
-		{5, 5},
-		{6, 3},
-		{7, 2},
-		{8, 3},
-		{10, 1},
-	}
+	// sk := []Tab{
+	// 	{1, 1},
+	// 	{2, 6},
+	// 	{4, 4},
+	// 	{3, 10},
+	// 	{5, 5},
+	// 	{6, 3},
+	// 	{7, 2},
+	// 	{8, 3},
+	// 	{10, 1},
+	// }
+	t.Log(h)
+	hrx := h.Get()
 	spolu := 0
-	ch := GenerujPresun(sk, 5)
+	ch := GenerujPresun(h.Presun(), 5)
 	for p := range ch {
 		spolu++
-		// fmt.Println(p)
+		h.Simul(p)
+		if hrx != h.Get() {
+			t.Log(h)
+			t.Fatalf("Excepted: %f, Have: %f", hrx, h.Get())
+		}
+
 	}
 	if spolu != 904 {
 		t.Fatalf("Excepted: 904, Have: %d", spolu)
 	}
 }
+
+var (
+	//Hrx
+	h *H = &H{
+		m:   35,
+		max: 10,
+		sk: map[int]int{
+			1:  1,
+			2:  6,
+			3:  10,
+			4:  4,
+			5:  5,
+			6:  3,
+			7:  2,
+			8:  3,
+			10: 1,
+		}}
+	//HHrx
+	h2 *H = &H{
+		m:   35,
+		max: 182,
+		sk: map[int]int{
+			122: 1,
+			131: 2,
+			132: 2,
+			135: 1,
+			137: 1,
+			138: 3,
+			139: 1,
+			140: 1,
+			141: 1,
+			142: 2,
+			144: 2,
+			147: 1,
+			148: 2,
+			149: 1,
+			150: 1,
+			152: 1,
+			154: 3,
+			155: 1,
+			157: 1,
+			158: 1,
+			159: 1,
+			166: 1,
+			167: 1,
+			169: 1,
+			170: 1,
+			182: 1,
+		}}
+)
+
+// func TestHrx(t *testing.T) {
+
+// p := Presun{
+// 	Tab{10, 1},
+// 	Tab{8, 3},
+// 	Tab{7, 1},
+// }
+// t.Log(h.Simul(p))
+// }
