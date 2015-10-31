8ef1f7e477f2ba0b10bb0806c7117598673f35de
Martin Eliáš <melias122@gmail.com>
2015-10-31 11:18:35 +0100

    Pridanie modu: range, exact pre Cislovacky + Zh

--- filter/cislovacky.go
+++ filter/cislovacky.go
@@ -2,53 +2,83 @@ package filter
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/melias122/psl/hrx"
 	"github.com/melias122/psl/komb"
+	"github.com/melias122/psl/num"
 )
 
-type cislovacky struct {
-	n        int
-	min, max int
-	fun      func(int) bool
-	fname    string
+// Cislovacky implementuju Filter pre P, N, Pr, Mc, Vc, C19, C0, cC, Cc, CC
+type Cislovacky struct {
+	n, min, max int
+	fname       string
+	f           num.CislovackaFunc
+	exact       []bool
 }
 
-func NewCislovacky(n, min, max int, fun func(int) bool, fname string) Filter {
+func CislovackyRange(n, min, max int, c num.Cislovacka) *Cislovacky {
 	if min < 0 {
 		min = 0
 	}
 	if max > n {
 		max = n
 	}
-	return cislovacky{
+	return &Cislovacky{
 		n:     n,
 		min:   min,
 		max:   max,
-		fun:   fun,
-		fname: fname,
+		f:     c.Func(),
+		fname: c.String(),
 	}
 }
 
-// IsP, IsN, IsPr, IsMc, IsVc, IsC19, IsC0, IscC, IsCc, IsCC
-func (c cislovacky) String() string {
+func CislovackyExact(n int, ints []int, c num.Cislovacka) *Cislovacky {
+	sort.Ints(ints)
+	min := ints[0]
+	max := ints[len(ints)-1]
+	if min < 0 {
+		min = 0
+	}
+	if max > n {
+		max = n
+	}
+	exact := make([]bool, n+1)
+	for _, i := range ints {
+		if i >= 0 && i <= n {
+			exact[i] = true
+		}
+	}
+	return &Cislovacky{
+		n:     n,
+		min:   min,
+		max:   max,
+		f:     c.Func(),
+		fname: c.String(),
+		exact: exact,
+	}
+}
+
+func (c *Cislovacky) String() string {
 	return fmt.Sprintf("%s: %d-%d", c.fname, c.min, c.max)
 }
 
-func (c cislovacky) Check(k komb.Kombinacia) bool {
+func (c *Cislovacky) Check(k komb.Kombinacia) bool {
 	var count int
-	for _, number := range k {
-		if c.fun(int(number)) {
+	for _, n := range k {
+		if c.f(int(n)) {
 			count++
 		}
 	}
-
 	if count > c.max || (len(k) == c.n && count < c.min) {
 		return false
+	}
+	if c.exact != nil && len(k) == c.n {
+		return c.exact[count]
 	}
 	return true
 }
 
-func (c cislovacky) CheckSkupina(skupina hrx.Skupina) bool {
+func (c *Cislovacky) CheckSkupina(skupina hrx.Skupina) bool {
 	return true
 }
--- filter/zhoda.go
+++ filter/zhoda.go
@@ -2,12 +2,13 @@ package filter
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/melias122/psl/hrx"
 	"github.com/melias122/psl/komb"
 )
 
-func NewZhoda(n, min, max int, kombinacia komb.Kombinacia) Filter {
+func ZhodaRange(n, min, max int, kombinacia komb.Kombinacia) Filter {
 	if min < 0 {
 		min = 0
 	}
@@ -22,15 +23,44 @@ func NewZhoda(n, min, max int, kombinacia komb.Kombinacia) Filter {
 	}
 }
 
+func ZhodaExact(n int, ints []int, kombinacia komb.Kombinacia) Filter {
+	sort.Ints(ints)
+	min := ints[0]
+	max := ints[len(ints)-1]
+	if min < 0 {
+		min = 0
+	}
+	if max > n {
+		max = n
+	}
+	exact := make([]bool, n+1)
+	for _, i := range ints {
+		if i >= 0 && i <= n {
+			exact[i] = true
+		}
+	}
+	return zhoda{
+		n:          n,
+		min:        min,
+		max:        max,
+		kombinacia: kombinacia,
+		exact:      exact,
+	}
+}
+
 type zhoda struct {
 	n, min, max int
 	kombinacia  komb.Kombinacia
+	exact       []bool
 }
 
-func (z zhoda) Check(kombinacia komb.Kombinacia) bool {
-	count := komb.Zhoda(z.kombinacia, kombinacia)
-	if (len(kombinacia) == z.n && count < z.min) || count > z.max {
+func (z zhoda) Check(k komb.Kombinacia) bool {
+	count := komb.Zhoda(z.kombinacia, k)
+	if (len(k) == z.n && count < z.min) || count > z.max {
 		return false
+	}
+	if z.exact != nil && len(k) == z.n {
+		return z.exact[count]
 	}
 	return true
 }
