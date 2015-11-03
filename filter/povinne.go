a7bc860fe44e7743253dc70dd9734dbe881460a8
Martin Eliáš <melias122@gmail.com>
2015-11-03 11:04:43 +0100

    Povinne: rewrite

--- filter/povinne.go
+++ filter/povinne.go
@@ -1,102 +1,159 @@
 package filter
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/melias122/psl/hrx"
 	"github.com/melias122/psl/komb"
+	"github.com/melias122/psl/parser"
 )
 
 type povinne struct {
-	n int
-	p map[byte]bool
+	povinne  []bool
+	n, count int
 }
 
-func NewPovinne(n int, cisla []byte) Filter {
-	p := make(map[byte]bool, len(cisla))
-	for _, cislo := range cisla {
-		p[cislo] = true
+func PovinneFromString(s string, last komb.Kombinacia, n, m int) (Filter, error) {
+	p := parser.NewParser(strings.NewReader(s), n, m)
+	p.Zhoda = last
+	ints, err := p.ParseInts()
+	if err != nil {
+		return nil, err
 	}
-	return povinne{
-		n: n,
-		p: p,
+	return Povinne(ints, n, m), nil
+}
+
+func Povinne(ints []int, n, m int) Filter {
+	p := povinne{
+		povinne: make([]bool, m),
+		n:       n,
 	}
+	for _, i := range ints {
+		if i >= 1 && i <= m && !p.povinne[i-1] {
+			p.povinne[i-1] = true
+			p.count++
+		}
+	}
+	if p.count > n {
+		p.count = n
+	}
+	return p
 }
 
 func (p povinne) Check(k komb.Kombinacia) bool {
+	// if p.n-len(k) > p.count {
+	// if len(k) < p.count {
+	// 	return true
+	// }
+
 	var count int
-	for _, cislo := range k {
-		if p.p[cislo] {
+	for _, i := range k {
+		if p.povinne[i-1] {
 			count++
 		}
 	}
-	var expected int
-	if len(p.p) > p.n {
-		expected = p.n
-	} else {
-		expected = len(p.p)
-	}
-	if len(k) == p.n && expected != count {
-		return false
+	if len(k) == p.n {
+		return p.count == count
 	}
 	return true
 }
 
-func (p povinne) CheckSkupina(skupina hrx.Skupina) bool {
+func (p povinne) CheckSkupina(s hrx.Skupina) bool {
 	return true
 }
 
 func (p povinne) String() string {
-	var b []byte
-	for k := range p.p {
-		b = append(b, k)
+	var s []string
+	for i, ok := range p.povinne {
+		if ok {
+			s = append(s, strconv.Itoa(i+1))
+		}
 	}
-	return fmt.Sprintf("Povinne: %s", strings.Replace(fmt.Sprintf("%v", b), " ", ", ", -1))
+	return "Povinne: " + strings.Join(s, ", ")
 }
 
 type povinneStl struct {
-	n       int
-	povinne map[stlKey]bool
-	stl     []bool
+	povinne [][]bool
+	count   int
 }
 
-func NewPovinneStl(n int, cisla [][]byte) Filter {
-	p := povinneStl{
-		n:       n,
-		povinne: make(map[stlKey]bool),
-		stl:     make([]bool, n),
+func PovinneSTLFromString(s string, zhoda []byte, n, m int) (Filter, error) {
+	parser := parser.NewParser(strings.NewReader(s), n, m)
+	parser.Zhoda = zhoda
+	ma, err := parser.ParseMapInts()
+	if err != nil {
+		return nil, err
 	}
-	for i := range cisla {
-		for _, c := range cisla[i] {
-			p.povinne[stlKey{STL: i, Cislo: c}] = true
-			p.stl[i] = true
+	// TODO: remove this conversion
+	var mapInts map[int][]int
+	for k, v := range ma {
+		mapInts[k] = v
+	}
+	return PovinneSTL(mapInts, n, m), nil
+}
+
+func PovinneSTL(mapInts map[int][]int, n, m int) Filter {
+	p := povinneStl{
+		povinne: make([][]bool, n),
+	}
+
+	for i, ints := range mapInts {
+		for _, j := range ints {
+			if !isOkForSTL(j-1, i-1, n, m) {
+				continue
+			}
+			// make slice
+			if p.povinne[i-1] == nil {
+				p.povinne[i-1] = make([]bool, m)
+				p.count++
+			}
+			// set if not seted
+			if !p.povinne[i-1][j-1] {
+				p.povinne[i-1][j-1] = true
+			}
 		}
 	}
 	return p
 }
 
 func (p povinneStl) Check(k komb.Kombinacia) bool {
-	for i, c := range k {
-		if ok := p.povinne[stlKey{STL: i, Cislo: c}]; !ok && p.stl[i] {
+	for i, j := range k {
+		if p.povinne[i] == nil {
+			continue
+		}
+		if !p.povinne[i][j-1] {
 			return false
 		}
 	}
 	return true
 }
 
-func (p povinneStl) CheckSkupina(skupina hrx.Skupina) bool {
+func (p povinneStl) CheckSkupina(s hrx.Skupina) bool {
 	return true
 }
 
 func (p povinneStl) String() string {
-	b := make([][]byte, p.n)
-	for k := range p.povinne {
-		b[k.STL] = append(b[k.STL], k.Cislo)
+	var s []string
+	for i := range p.povinne {
+		if p.povinne[i] == nil {
+			continue
+		}
+		var ints []string
+		for j, ok := range p.povinne[i] {
+			if ok {
+				ints = append(ints, strconv.Itoa(j+1))
+			}
+		}
+		s = append(s, strconv.Itoa(i+1)+":"+strings.Join(ints, ", "))
 	}
-	var s string
-	for i := range b {
-		s += fmt.Sprintf("%d:%s ", i+1, strings.Replace(fmt.Sprintf("%v", b[i]), " ", ", ", -1))
+	return "Povinne STL: " + strings.Join(s, "; ")
+}
+
+func isOkForSTL(number, STL, n, m int) bool {
+	// 2:1; 3:1,2; ... || 1:32,33,...; 2:33,...
+	if number < STL || number > (m-n+STL) {
+		return false
 	}
-	return "Povinne STL: " + s
+	return true
 }
