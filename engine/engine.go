480548090ca275832ff8be2c0c8cbe85527263e9
Martin Elias <melias122@gmail.com>
2017-06-05 15:11:42 +0200

    engine: definovanie zakldanych interface

--- engine/engine.go
+++ engine/engine.go
@@ -1,13 +1,68 @@
 package engine
 
-type Parser interface {
-	Parse() ([]Kombinacia, error)
+// Kombinacia reprezentuje kombinaciu cisiel.
+// Cisla musia byt vacsie ako 1 a mensie ako 'm'. Velkost kombinacie musi byt 'n'
+// a nesmie sa menit.
+type Kombinacia []int
+
+// Rc je cislo c. c moze mat hodnotu od 1 do n.
+type Rc interface {
+	// Rp vrati pocetnost cisla c.
+	Rp(c int) int
+
+	// Rh vrati hodnotu cisla c vypocitanu pomocou vzorca H(c, 1, Rp(c), n, m).
+	Rh(c int) float64
 }
 
-type (
-	Kombinacia []int
+// STLc je cislo c v stlpci s.
+type STLc interface {
+	// STLp vrati pocetnost cisla c.
+	STLp(c int, s int) int
 
-	Cislovacka int
+	// STLh vypocita hodnotu cisla pomocou vzorca H(c, s, STLp(c), n, m).
+	STLh(c int, s int) float64
+}
 
-	Cifrovacky [10]byte
-)
+// Cislo je cislo v riadku a slpci s hodnotami R a STL.
+type Cislo interface {
+	Rc
+	STLc
+}
+
+// Rk definuje sucet hodnot Rp.
+type Rk interface {
+	// R vrati sucet hodnot Rp Kombinacie.
+	R(Kombinacia) float64
+}
+
+// STLk definuje sucet hodnot STLp.
+type STLk interface {
+	// STL vrati sucet hodnot STLp Kombinacie.
+	STL(Kombinacia) float64
+}
+
+// RSTLk kombinuje sucty R a STL.
+type RSTLk interface {
+	Rk
+	STLk
+}
+
+// Xk vypocita hodnotu Hrx, pripadne HHrx.
+type Xk interface {
+	// Ak je zadana Kombinacia k vypocita hodnotu po presune cisiel.
+	// V pripade k == nil, vrati aktualnu hodnotu hrx, hhrx.
+	X(k Kombinacia) float64
+}
+
+// Hrx je rozhranie ktore zdruzuje hodnoty R a STL vypocitane pre 1-DO a OD-DO.
+type Hrx interface {
+	Cislo
+	RSTLk
+	Xk
+}
+
+// Uc je uzatvaracie cislo pre OD-DO.
+type Uc struct {
+	Riadok int
+	Cislo  int
+}
