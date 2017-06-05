b63adc499d257a794010a31347d114c9d413bede
Martin Elias <melias122@gmail.com>
2017-06-05 15:05:17 +0200

    engine: vycistenie cifrovaciek

--- engine/cifrovacka.go
+++ engine/cifrovacka.go
@@ -1,44 +1,55 @@
 package engine
 
-import (
-	"strconv"
-	"strings"
-)
+type Cifrovacka struct {
+	C1 byte
+	C2 byte
+	C3 byte
+	C4 byte
+	C5 byte
+	C6 byte
+	C7 byte
+	C8 byte
+	C9 byte
+	C0 byte
+}
 
-func CifrovackyTeorMax(n, m int) Cifrovacky {
-	var c Cifrovacky
+func NewCifrovacka(k Kombinacia) Cifrovacka {
+	var c Cifrovacka
+	for _, n := range k {
+		c.set(n)
+	}
+	return c
+}
+
+func NewCifrovackaMax(n, m int) Cifrovacka {
+	var c Cifrovacka
 	for i := 1; i <= m; i++ {
 		c.set(i)
 	}
 	return c
 }
 
-func NewCifrovacky(k Kombinacia) Cifrovacky {
-	var c Cifrovacky
-	for _, n := range k {
-		c.set(int(n))
-	}
-	return c
-}
-
-func (c *Cifrovacky) set(n int) {
-	n = n % 10
-	switch n {
+func (c *Cifrovacka) set(n int) {
+	switch n % 10 {
+	case 1:
+		c.C1++
+	case 2:
+		c.C2++
+	case 3:
+		c.C3++
+	case 4:
+		c.C4++
+	case 5:
+		c.C5++
+	case 6:
+		c.C6++
+	case 7:
+		c.C7++
+	case 8:
+		c.C8++
+	case 9:
+		c.C9++
 	case 0:
-		c[9]++
-	default:
-		c[n-1]++
+		c.C0++
 	}
-}
-
-func (c Cifrovacky) Strings() []string {
-	s := make([]string, len(c))
-	for i, c := range c {
-		s[i] = strconv.Itoa(int(c))
-	}
-	return s
-}
-
-func (c Cifrovacky) String() string {
-	return strings.Join(c.Strings(), " ")
 }
--- engine/cifrovacka_test.go
+++ engine/cifrovacka_test.go
@@ -1,39 +1,38 @@
 package engine
 
 import (
-	"bytes"
 	"testing"
 )
 
-func TestCifrovackyTeorMax(t *testing.T) {
+func TestCifrovackaTeorMax(t *testing.T) {
 	tests := []struct {
 		n, m int
-		w    Cifrovacky
+		w    Cifrovacka
 	}{
-		{5, 35, Cifrovacky{4, 4, 4, 4, 4, 3, 3, 3, 3, 3}},
-		{5, 90, Cifrovacky{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}},
+		{5, 35, Cifrovacka{4, 4, 4, 4, 4, 3, 3, 3, 3, 3}},
+		{5, 90, Cifrovacka{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}},
 	}
 	for _, test := range tests {
-		got := CifrovackyTeorMax(test.n, test.m)
-		if bytes.Compare(got[:], test.w[:]) != 0 {
+		got := NewCifrovackaMax(test.n, test.m)
+		if test.w != got {
 			t.Errorf("Expected %v, got: %v", test.w, got)
 		}
 	}
 }
 
-func TestNewCifrovacky(t *testing.T) {
+func TestNewCifrovacka(t *testing.T) {
 	tests := []struct {
 		k Kombinacia
-		w Cifrovacky
+		w Cifrovacka
 	}{
-		{k: Kombinacia{}, w: Cifrovacky{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
-		{k: Kombinacia{1, 2, 3, 4, 5}, w: Cifrovacky{1, 1, 1, 1, 1, 0, 0, 0, 0, 0}},
-		{k: Kombinacia{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, w: Cifrovacky{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
-		{k: Kombinacia{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, w: Cifrovacky{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
+		{k: Kombinacia{}, w: Cifrovacka{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{k: Kombinacia{1, 2, 3, 4, 5}, w: Cifrovacka{1, 1, 1, 1, 1, 0, 0, 0, 0, 0}},
+		{k: Kombinacia{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, w: Cifrovacka{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
+		{k: Kombinacia{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, w: Cifrovacka{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
 	}
 	for _, test := range tests {
-		c := NewCifrovacky(test.k)
-		if bytes.Compare(c[:], test.w[:]) != 0 {
+		c := NewCifrovacka(test.k)
+		if c != test.w {
 			t.Errorf("Expected %v, got: %v", test.w, c)
 		}
 	}
