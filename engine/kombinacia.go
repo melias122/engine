e44aaa7fe4b80c9da2c226c776854bef1e508864
Martin Elias <melias122@gmail.com>
2017-06-05 15:04:21 +0200

    engine: zmazanie Sucet, Sled

--- engine/kombinacia.go
+++ engine/kombinacia.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"strconv"
-	"strings"
 )
 
 func (k Kombinacia) String() string {
@@ -22,84 +21,4 @@ func (k Kombinacia) Sucet() int {
 		sucet += int(cislo)
 	}
 	return sucet
-}
-
-func (k Kombinacia) SucetRS(n Nums) (float64, float64) {
-	var r, s float64
-	for i, cislo := range k {
-		if n[cislo-1] != nil {
-			r += n[cislo-1].R()
-			s += n[cislo-1].S(i + 1)
-		}
-	}
-	return r, s
-}
-
-func (k Kombinacia) SucetRSNext(n Nums) (float64, float64) {
-	var r, s float64
-	for i, cislo := range k {
-		if n[cislo-1] != nil {
-			r += n[cislo-1].RNext()
-			s += n[cislo-1].SNext(i + 1)
-		}
-	}
-	return r, s
-}
-
-func (k Kombinacia) SledPN() string {
-	s := make([]string, len(k))
-	for i, j := range k {
-		if IsP(int(j)) {
-			s[i] = "P"
-		} else {
-			s[i] = "N"
-		}
-	}
-	return strings.Join(s, " ")
-}
-
-func (k Kombinacia) SledPNPr() string {
-	s := make([]string, len(k))
-	for i, j := range k {
-		j := int(j)
-		if IsPr(j) {
-			s[i] = "Pr"
-		} else if IsN(j) {
-			s[i] = "N"
-		} else {
-			s[i] = "P"
-		}
-	}
-	return strings.Join(s, " ")
-}
-
-func (k Kombinacia) SledMcVc() string {
-	s := make([]string, len(k))
-	for i, j := range k {
-		if IsMc(int(j)) {
-			s[i] = "Mc"
-		} else {
-			s[i] = "Vc"
-		}
-	}
-	return strings.Join(s, " ")
-}
-
-func (k Kombinacia) SledPrirodzene() string {
-	s := make([]string, len(k))
-	for i, j := range k {
-		j := int(j)
-		if IsC19(j) {
-			s[i] = "C19"
-		} else if IsC0(j) {
-			s[i] = "C0"
-		} else if IscC(j) {
-			s[i] = "cC"
-		} else if IsCc(j) {
-			s[i] = "Cc"
-		} else { // CC
-			s[i] = "CC"
-		}
-	}
-	return strings.Join(s, " ")
 }
