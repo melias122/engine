51096a89d7958b0d1c1983e9d2cecaf88a80043b
Martin Elias <melias122@gmail.com>
2018-02-23 20:29:37 +0100

    engine: remove TestXcislaConstains from xtica

--- engine/xtica_test.go
+++ engine/xtica_test.go
@@ -4,33 +4,6 @@ import (
 	"testing"
 )
 
-func TestXcislaContains(t *testing.T) {
-	tests := []struct {
-		xcisla Xcisla
-		test   map[Tab]bool
-	}{
-		{xcisla: Xcisla{}, test: map[Tab]bool{Tab{}: false, Tab{1, 1}: false}},
-		{xcisla: Xcisla{{0, 0}}, test: map[Tab]bool{Tab{0, 0}: true, Tab{1, 1}: false}},
-		{
-			xcisla: Xcisla{
-				{1, 1}, {2, 2}, {4, 0},
-			},
-			test: map[Tab]bool{
-				Tab{0, 0}: false, Tab{1, 1}: true, Tab{2, 2}: true, Tab{3, 3}: false,
-				Tab{3, 0}: false, Tab{3, 1}: false, Tab{4, 0}: true, Tab{4, 1}: false,
-				Tab{5, 1}: false,
-			},
-		},
-	}
-	for _, test := range tests {
-		for tab, exp := range test.test {
-			if ok := test.xcisla.Contains(tab); ok != exp {
-				t.Fatalf("Expected: (%v:%v), got: (%v:%v)", tab, exp, test.xcisla, ok)
-			}
-		}
-	}
-}
-
 func TestXtica(t *testing.T) {
 	tests := []struct {
 		m int
